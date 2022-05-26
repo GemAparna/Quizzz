7c50c948a6c832f34939e4bfa15d832b64edc092
GemAparna <Aparna.Singh@Geminisolutions.com>
2022-05-26 20:17:01 +0530

    yes

--- my-app/src/components/Details.js
+++ my-app/src/components/Details.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Next from './Next'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Next from './Next';
 
 
 const Details = ({ details, setDetails, crr_option, setCrr_option }) => {
